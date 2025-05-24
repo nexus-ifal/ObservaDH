2c79776df05708acf5e7ab291c0f96f4c9ae0fa9
follijulio <follijulio@gmail.com>
2025-05-24 20:28:32 -0300

    feat: adicionar QueryProvider para gerenciamento de estado com React Query

---
+++ src/components/ui/layouts/query-provider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode, useState } from "react";
+
+export default function QueryProvider({ children }: { children: ReactNode }) {
+	const [queryClient] = useState(
+		() =>
+			new QueryClient({
+				defaultOptions: {
+					queries: {
+						retry: false,
+						retryOnMount: false,
+						refetchOnWindowFocus: false,
+						staleTime: 15 * 60 * 1000,
+					},
+				},
+			})
+	);
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			{children}
+		</QueryClientProvider>
+	);
+}
