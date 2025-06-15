9070354dfa8996a01f5e10db0f67b35ba2c54acc
follijulio <follijulio@gmail.com>
2025-06-15 09:12:54 -0300

    feat: implementa CRUD para Projeto com hooks e opções correspondentes

--- src/core/interfaces/repository/projeto.repository.ts
+++ src/core/interfaces/repository/projeto.repository.ts
@@ -1,5 +1,12 @@
-import { ResponseProjetoDTO } from "@/core/domain/dtos/projeto.dto";
+import {
+	CreateProjetoDTO,
+	ResponseProjetoDTO,
+	UpdateProjetoDTO,
+} from "@/core/domain/dtos/projeto.dto";
 
 export interface ProjetoRepository {
 	listar(): Promise<ResponseProjetoDTO[]>;
+	criar(projeto: CreateProjetoDTO): Promise<ResponseProjetoDTO>;
+	atualizar(id: string, projeto: UpdateProjetoDTO): Promise<ResponseProjetoDTO>;
+	excluir(id: string): Promise<ResponseProjetoDTO>;
 }
--- src/core/services/projeto.service.ts
+++ src/core/services/projeto.service.ts
@@ -1,4 +1,8 @@
-import { ResponseProjetoDTO } from "../domain/dtos/projeto.dto";
+import {
+	CreateProjetoDTO,
+	ResponseProjetoDTO,
+	UpdateProjetoDTO,
+} from "../domain/dtos/projeto.dto";
 import { ProjetoRepository } from "../interfaces/repository/projeto.repository";
 import ProjetoUseCase from "../interfaces/usecase/projeto.usecase";
 
@@ -11,6 +15,21 @@ class ProjetoService implements ProjetoUseCase {
 
 	listar(): Promise<ResponseProjetoDTO[]> {
 		return this.adapter.listar();
+	}
+
+	criar(projeto: CreateProjetoDTO): Promise<ResponseProjetoDTO> {
+		return this.adapter.criar(projeto);
+	}
+
+	atualizar(
+		id: string,
+		projeto: UpdateProjetoDTO
+	): Promise<ResponseProjetoDTO> {
+		return this.adapter.atualizar(id, projeto);
+	}
+
+	excluir(id: string): Promise<ResponseProjetoDTO> {
+		return this.adapter.excluir(id);
 	}
 }
 
