66a78aa57fb80b9ed258323b0f72744842140c27
follijulio <follijulio@gmail.com>
2025-06-15 09:41:59 -0300

    feat: implementa operações CRUD para Politico com hooks e opções correspondentes

--- src/core/interfaces/repository/politico.repository.ts
+++ src/core/interfaces/repository/politico.repository.ts
@@ -1,5 +1,15 @@
-import { ResponsePoliticoDTO } from "@/core/domain/dtos/politico.dto";
+import {
+	CreatePoliticoDTO,
+	ResponsePoliticoDTO,
+	UpdatePoliticoDTO,
+} from "@/core/domain/dtos/politico.dto";
 
 export interface PoliticoRepository {
 	listar(): Promise<ResponsePoliticoDTO[]>;
+	criar(politico: CreatePoliticoDTO): Promise<ResponsePoliticoDTO>;
+	atualizar(
+		id: string,
+		politico: UpdatePoliticoDTO
+	): Promise<ResponsePoliticoDTO>;
+	excluir(id: string): Promise<ResponsePoliticoDTO>;
 }
