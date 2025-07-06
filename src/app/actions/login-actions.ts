2e874a97919bef00b556a08155c3ef47560b78f0
Rita <rk7.w2855b@gmail.com>
2025-07-05 22:01:06 -0300

    refactor: remover importação não utilizada e melhorar formatação do código

--- src/app/actions/login-actions.ts
+++ src/app/actions/login-actions.ts
@@ -1,6 +1,5 @@
 "use server";
 
-import { Role } from "@prisma/client";
 import { redirect } from "next/navigation";
 import { AuthError } from "next-auth";
 import { ZodError } from "zod";
@@ -26,7 +25,8 @@ export async function authenticate(
 	}
 
 	const { email, password } = camposValidados.data;
-	const redirectTo = formData.get("redirectTo") as string || "/user-routes/home";
+	const redirectTo =
+		(formData.get("redirectTo") as string) || "/user-routes/home";
 
 	try {
 		await signIn("credentials", {
@@ -43,19 +43,23 @@ export async function authenticate(
 
 		if (error instanceof AuthError) {
 			switch (error.type) {
-                case "CredentialsSignin":
-                    console.error("Erro de credenciais inválidas:", error);
-                    return "Credenciais inválidas ou e-mail não verificado.";
-                case "CallbackRouteError":
-                    if (error.cause?.err?.message.includes("e-mail ainda não foi verificado")) {
-                        return "Seu e-mail ainda não foi verificado. Por favor, verifique sua caixa de entrada.";
-                    }
-                    console.error("Erro no callback do Auth.js:", error);
-                    return "Ocorreu um erro desconhecido durante o login. Tente novamente.";
-                default:
-                    console.error("Ocorreu um erro de autenticação desconhecido:", error);
-                    return "Ocorreu um erro desconhecido durante o login.";
-            }
+				case "CredentialsSignin":
+					console.error("Erro de credenciais inválidas:", error);
+					return "Credenciais inválidas ou e-mail não verificado.";
+				case "CallbackRouteError":
+					if (
+						error.cause?.err?.message.includes(
+							"e-mail ainda não foi verificado"
+						)
+					) {
+						return "Seu e-mail ainda não foi verificado. Por favor, verifique sua caixa de entrada.";
+					}
+					console.error("Erro no callback do Auth.js:", error);
+					return "Ocorreu um erro desconhecido durante o login. Tente novamente.";
+				default:
+					console.error("Ocorreu um erro de autenticação desconhecido:", error);
+					return "Ocorreu um erro desconhecido durante o login.";
+			}
 		}
 		if (error instanceof ZodError) {
 			const mensagensDeErro = Object.values(error.flatten().fieldErrors).flat();
